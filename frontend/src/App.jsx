027e1eb290411c040913fb86e1a0a6ac58a1b64f
DAX1207 <daksh12.vyas@gmail.com>
2023-12-16 16:39:26 -0500

    d1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import GroceriesApp from "./GroceriesApp";
+import loginUser from "./pages/loginUser";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <GroceriesApp />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/main" element={<GroceriesApp />} />
+          <Route path="/" element={<loginUser />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
