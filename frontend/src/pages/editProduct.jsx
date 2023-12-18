5107b711527d68d8aac281f23f41a77881c6d20b
rahulnolee <rahulparmar8285@gmail.com>
2023-12-17 20:11:32 -0500

    y

--- frontend/src/pages/editProduct.jsx
+++ frontend/src/pages/editProduct.jsx
@@ -4,7 +4,6 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
 
 export default function EditProduct() {
-<<<<<<< HEAD
     const [searchParams] = useSearchParams();
     const navigate = useNavigate();
     const [formData, setFormData] = useState({
@@ -14,42 +13,10 @@ export default function EditProduct() {
         quantity: "",
         image: "",
         price: "",
-=======
-  const [searchParams, setSearchParams] = useSearchParams();
-  const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    id: "",
-    productName: "",
-    brand: "",
-    quantity: "",
-    image: "",
-    price: "",
-  });
-
-  const [postResponse, setPostResponse] = useState("");
-
-  async function handleFindDB() {
-    console.log;
-    const id = searchParams.get("id");
-    const response = await fetch(`http://localhost:3000/product/${id}`);
-    console.log("res", response);
-    const product = await response.json();
-    setFormData({
-      id: product[0].id,
-      productName: product[0].productName,
-      brand: product[0].brand,
-      quantity: product[0].quantity,
-      image: product[0].image,
-      price: product[0].price,
->>>>>>> eee988cf8604f6e6b9ccf2d1d7a17e031e081bac
     });
-  }
 
-  useEffect(() => {
-    handleFindDB();
-  }, []);
+    const [postResponse, setPostResponse] = useState("");
 
-<<<<<<< HEAD
     async function handleFindDB() {
         const id = searchParams.get("id");
         const response = await fetch(`http://localhost:3000/product/${id}`);
@@ -97,125 +64,91 @@ export default function EditProduct() {
             [fieldName]: fieldValue,
           };
         });
-=======
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    defaultValues: formData.id
-      ? formData
-      : {
-          id: " ",
-          productName: " ",
-          brand: " ",
-          quantity: " ",
-          image: " ",
-          price: " ",
-        },
-  });
-
-  const handleOnChange = (evt) => {
-    const fieldName = evt.target.name;
-    const fieldValue = evt.target.value;
-    setFormData((prevData) => {
-      return {
-        ...prevData,
-        id: crypto.randomUUID(),
-        [fieldName]: fieldValue,
->>>>>>> eee988cf8604f6e6b9ccf2d1d7a17e031e081bac
       };
-    });
-  };
 
-  const handleOnSubmit = async (evt) => {
-    evt.preventDefault;
-    setPostResponse("");
-    const id = searchParams.get("id");
-    await axios
-      .patch(`http://localhost:3000/products/${id}`, formData)
-      .then((response) => setPostResponse(<p>{response.data}</p>));
-  };
+    const handleOnSubmit = async (evt) => {
+        evt.preventDefault;
+        setPostResponse("");
+        const id = searchParams.get("id");
+        await axios
+            .patch(`http://localhost:3000/products/${id}`, formData)
+            .then((response) => setPostResponse(<p>{response.data}</p>))
+    };
 
-  return (
-    <div className="custom-form-container">
-      <form action="" onSubmit={handleSubmit(handleOnSubmit)}>
-        <div className="custom-form-input">
-          <label htmlFor="productName">Product Name</label>
-          <input
-            type="text"
-            {...register("productName", {
-              required: "Please provide a product name",
-            })}
-            id="productName"
-            onChange={handleOnChange}
-            value={formData.productName}
-          />
-          <p>{errors.productName?.message}</p>
+    return (
+        <div className="custom-form-container">
+            <form action="" onSubmit={handleSubmit(handleOnSubmit)}>
+                <div className="custom-form-input">
+                    <label htmlFor="productName">Product Name</label>
+                    <input
+                        type="text"
+                        {...register("productName", {
+                        required: "Please provide a product name",
+                        })}
+                        id="productName"
+                        onChange={handleOnChange}
+                        value={formData.productName}
+                    />
+                    <p>{errors.productName?.message}</p>
+                </div>
+                    <div className="custom-form-input">
+                    <label htmlFor="brand">Brand</label>
+                    <input
+                        type="text"
+                        {...register("brand", {
+                        required: "Please provide a brand name",
+                        })}
+                        id="brand"
+                        onChange={handleOnChange}
+                        value={formData.brand}
+                    />
+                    <p>{errors.brand?.message}</p>
+                </div>
+                <div className="custom-form-input">
+                    <label htmlFor="quantity">Quantity</label>
+                    <input
+                        type="text"
+                        {...register("quantity", {
+                        required: "Please provide a quantity",
+                        })}
+                        id="quantity"
+                        onChange={handleOnChange}
+                        value={formData.quantity}
+                    />
+                    <p>{errors.quantity?.message}</p>
+                </div>
+                <div className="custom-form-input">
+                    <label htmlFor="image">Image Link</label>
+                    <input
+                        type="text"
+                        {...register("image", {
+                        required: "Please provide a image URL",
+                        })}
+                        id="image"
+                        onChange={handleOnChange}
+                        value={formData.image}
+                    />
+                    <p>{errors.image?.message}</p>
+                </div>
+                <div className="custom-form-input">
+                    <label htmlFor="price">Price</label>
+                    <input
+                        type="text"
+                        {...register("price", {
+                        required: "Please provide a price",
+                        })}
+                        id="price"
+                        onChange={handleOnChange}
+                        value={formData.price}
+                    />
+                    <p>{errors.price?.message}</p>
+                </div>
+                <button className="custom-form-submit-btn">Edit Product</button>
+            </form>
+            <div className="result-panel">{ postResponse ? `${formData.productName} is edited` : ""}</div>
+            <button className="custom-form-submit-btn back-to-inventory" onClick={() => navigate("/main")}>
+                Back to Inventory
+            </button>
         </div>
-        <div className="custom-form-input">
-          <label htmlFor="brand">Brand</label>
-          <input
-            type="text"
-            {...register("brand", {
-              required: "Please provide a brand name",
-            })}
-            id="brand"
-            onChange={handleOnChange}
-            value={formData.brand}
-          />
-          <p>{errors.brand?.message}</p>
-        </div>
-        <div className="custom-form-input">
-          <label htmlFor="quantity">Quantity</label>
-          <input
-            type="text"
-            {...register("quantity", {
-              required: "Please provide a quantity",
-            })}
-            id="quantity"
-            onChange={handleOnChange}
-            value={formData.quantity}
-          />
-          <p>{errors.quantity?.message}</p>
-        </div>
-        <div className="custom-form-input">
-          <label htmlFor="image">Image Link</label>
-          <input
-            type="text"
-            {...register("image", {
-              required: "Please provide a image URL",
-            })}
-            id="image"
-            onChange={handleOnChange}
-            value={formData.image}
-          />
-          <p>{errors.image?.message}</p>
-        </div>
-        <div className="custom-form-input">
-          <label htmlFor="price">Price</label>
-          <input
-            type="text"
-            {...register("price", {
-              required: "Please provide a price",
-            })}
-            id="price"
-            onChange={handleOnChange}
-            value={formData.price}
-          />
-          <p>{errors.price?.message}</p>
-        </div>
-        <button className="custom-form-submit-btn">Edit Product</button>
-      </form>
-      <div className="result-panel">
-        {postResponse ? `${formData.productName} is edited` : ""}
-      </div>
-      <button
-        className="custom-form-submit-btn back-to-inventory"
-        onClick={() => navigate("/app")}
-      >
-        Back to Inventory
-      </button>
-    </div>
-  );
+    );
 }
